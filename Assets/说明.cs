9c9d365a27e2e44e73b6f5fec212ab3de824a185
黄 <59413749+ChrisZero890102@users.noreply.github.com>
2024-01-10 16:51:51 +0800

    上传一个标识文件

---
+++ Assets/说明.cs
@@ -0,0 +1,19 @@
+//此项目为2023年测试2D项目
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 说明 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
